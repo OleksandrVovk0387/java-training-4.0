6488e262054a06b6e6ccffcdbe8e2c143b82b754
OleksandrVovk0387 <sinoptik87@ulr.net>
2024-11-28 18:20:07 +0200

    HW#14

--- src/StreamTask.java
+++ src/StreamTask.java
@@ -1,12 +1,20 @@
+import java.util.Arrays;
+
 public class StreamTask {
 
     public static int getSumSquareEven(int[] numbers) {
-        // TODO replace return with your code
-        return 0;
+        // Перетворення масиву в потік, фільтрація парних чисел, піднесення до квадрату і сума
+        return Arrays.stream(numbers)
+                .filter(number -> number % 2 == 0) // Залишаємо лише парні числа
+                .map(number -> number * number) // Підносимо до квадрату
+                .sum(); // Підраховуємо суму
     }
 
     public static int[] getSortedOddsArray(int[] numbers) {
-        // TODO replace return with your code
-        return new int[]{};
+        // Перетворення масиву в потік, фільтрація непарних чисел, сортування і повернення масиву
+        return Arrays.stream(numbers)
+                .filter(number -> number % 2 != 0) // Залишаємо лише непарні числа
+                .sorted() // Сортуємо за зростанням
+                .toArray(); // Повертаємо як масив
     }
 }
